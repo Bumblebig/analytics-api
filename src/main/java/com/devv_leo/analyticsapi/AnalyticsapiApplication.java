bf7f2bad65bdea4fc619474bd938b6b503ed15de
Lekan Olaoye <lekan.code@gmail.com>
2026-02-21 11:44:56 +0100

    first init

---
+++ src/main/java/com/devv_leo/analyticsapi/AnalyticsapiApplication.java
@@ -0,0 +1,13 @@
+package com.devv_leo.analyticsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devv_leo/analyticsapi/AnalyticsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devv_leo.analyticsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
